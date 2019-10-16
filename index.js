19281babb21c727d53d0894169450d632caa3036
shivansh2598 <shivanshsinha2598@gmail.com>
2019-10-16 21:31:17 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const cv = require('opencv4nodejs')
+const path = require('path')
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const wCap = new cv.VideoCapture(0);
+
+const FPS=50;
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+
+setInterval(() => {
+    const frame = wCap.read();
+    const image = cv.imencode('.jpg', frame).toString('base64')
+    io.emit('image',image);
+}, 1000/FPS)
+
+server.listen(3000,()=>{
+    console.log("Server Started")
+});
